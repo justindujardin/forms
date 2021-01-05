3112ae773888389c4784f16b6d16a889a8eacef7
Justin DuJardin <justindujardin@users.noreply.github.com>
2021-01-05 13:40:05 -0800

    chore: add version ranges to help pip solver (#34)

--- libraries/python/tools/setup.sh
+++ libraries/python/tools/setup.sh
@@ -4,10 +4,11 @@ set -e
 # Make the virtualenv only if the folder doesn't exist
 DIR=.env
 if [ ! -d "${DIR}" ]; then
-  virtualenv .env -p python3.6
+  virtualenv .env -p python3
 fi
 
 . .env/bin/activate
+pip install --upgrade pip
 echo "Installing/updating requirements..."
 pip install -e .[dev]
 
